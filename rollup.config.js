b05b037d2b80ec3760bfb072edce2801024651f2
herr kaste <herr.kaste@gmail.com>
2017-10-23 13:41:16 +0200

    Towards ES6 modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,8 @@
-import legacy from 'rollup-plugin-legacy';
-
 export default {
-  input: 'hyperhtml.js',
+  input: 'hyperHTML.js',
   output: {
     file: 'index.js',
     format: 'iife',
     name: 'hyperHTML',
-  },
-  plugins: [
-      legacy({
-        './node_modules/majinbuu/index.js': 'majinbuu',
-      })
-    ]
+  }
 };
