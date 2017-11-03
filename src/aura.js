59be101f5d31f57645b5fbaf99ea370f38b2c644
herr kaste <herr.kaste@gmail.com>
2017-11-03 15:09:02 +0100

    Move majinbuu/aura to its own module

---
+++ src/aura.js
@@ -0,0 +1,74 @@
+import { majinbuu } from '../node_modules/majinbuu/esm/main.js'
+import { createDocumentFragment } from './dom-utils.js'
+
+// avoid processing too many nodes
+// this is about the algorithm used
+// to calculate the least amount of DOM
+// changes needed to show the a new list
+// where there was another one.
+// There is a limit, in terms of performance,
+// on how big can the optimal computation be,
+// so if you change this value be sure your
+// target hardware is good enough.
+const MAX_LIST_SIZE = 1000
+
+// ---------------------------------------------
+// DOM Manipulation
+// ---------------------------------------------
+
+export function Aura(node, childNodes) {
+  this.node = node
+  this.childNodes = childNodes
+  return majinbuu.aura(this, childNodes)
+}
+
+Aura.prototype.splice = function splice(start) {
+  for (
+    var tmp,
+      ph = this.node,
+      cn = this.childNodes,
+      target = cn[start + (arguments[1] || 0)] || ph,
+      result = cn.splice.apply(cn, arguments),
+      pn = ph.parentNode,
+      i = 0,
+      length = result.length;
+    i < length;
+    i++
+  ) {
+    tmp = result[i]
+    // TODO: this is not optimal (but necessary)
+    if (cn.indexOf(tmp) < 0) {
+      pn.removeChild(tmp)
+    }
+  }
+  i = 2
+  length = arguments.length
+  if (i < length) {
+    if (length - i === 1) {
+      tmp = arguments[i]
+    } else {
+      tmp = createDocumentFragment(pn.ownerDocument)
+      while (i < length) {
+        tmp.appendChild(arguments[i++])
+      }
+    }
+    pn.insertBefore(tmp, target)
+  }
+  return result
+}
+
+// uses majinbuu only if the two lists are different
+export function optimist(aura, value) {
+  var i = 0,
+    length = aura.length
+  if (value.length !== length) {
+    majinbuu(aura, value, MAX_LIST_SIZE)
+  } else {
+    for (; i < length--; i++) {
+      if (aura[length] !== value[length] || aura[i] !== value[i]) {
+        majinbuu(aura, value, MAX_LIST_SIZE)
+        return
+      }
+    }
+  }
+}
--- src/hyperhtml.js
+++ src/hyperhtml.js
@@ -1,11 +1,11 @@
 /* eslint semi: [1, 'always'] */
 
-import {majinbuu} from '../node_modules/majinbuu/esm/main.js';
 // import {makeAttributeUpdateFn} from './attribute-updater.js';
 import {
   makeRxAwareAttributeUpdateFn, rxAware
 } from './rx-aware-attribute-updater.js';
 import {createTemplateBlueprint} from './make-template-blueprints.js';
+import {Aura, optimist} from './aura.js';
 import {
   createDocumentFragment,
   createFragment,
@@ -22,7 +22,7 @@ import {$WeakMap} from './pseudo-polyfills.js';
 
 
 
-var hyperHTML = (function (globalDocument, majinbuu) {'use strict';
+var hyperHTML = (function (globalDocument) {'use strict';
 
   /*! (c) 2017 Andrea Giammarchi @WebReflection, (ISC) */
 
@@ -121,49 +121,7 @@ var hyperHTML = (function (globalDocument, majinbuu) {'use strict';
   var UID = EXPANDO + ((Math.random() * new Date) | 0) + ';';
   var UIDC = '<!--' + UID + '-->';
 
-  // ---------------------------------------------
-  // DOM Manipulation
-  // ---------------------------------------------
 
-  function Aura(node, childNodes) {
-    this.node = node;
-    this.childNodes = childNodes;
-    return majinbuu.aura(this, childNodes);
-  }
-
-  Aura.prototype.splice = function splice(start) {
-    for (var
-      tmp,
-      ph = this.node,
-      cn = this.childNodes,
-      target = cn[start + (arguments[1] || 0)] || ph,
-      result = cn.splice.apply(cn, arguments),
-      pn = ph.parentNode,
-      i = 0,
-      length = result.length;
-      i < length; i++
-    ) {
-      tmp = result[i];
-      // TODO: this is not optimal (but necessary)
-      if (cn.indexOf(tmp) < 0) {
-        pn.removeChild(tmp);
-      }
-    }
-    i = 2;
-    length = arguments.length;
-    if (i < length) {
-      if ((length - i) === 1) {
-        tmp = arguments[i];
-      } else {
-        tmp = createDocumentFragment(pn.ownerDocument);
-        while (i < length) {
-          tmp.appendChild(arguments[i++]);
-        }
-      }
-      pn.insertBefore(tmp, target);
-    }
-    return result;
-  };
 
   // ---------------------------------------------
   // hyperHTML Operations
@@ -389,20 +347,6 @@ var hyperHTML = (function (globalDocument, majinbuu) {'use strict';
     return value != null && 'then' in value;
   }
 
-  // uses majinbuu only if the two lists are different
-  function optimist(aura, value) {
-    var i = 0, length = aura.length;
-    if (value.length !== length) {
-      majinbuu(aura, value, hyper.MAX_LIST_SIZE);
-    } else {
-      for (; i < length--; i++) {
-        if (aura[length] !== value[length] || aura[i] !== value[i]) {
-          majinbuu(aura, value, hyper.MAX_LIST_SIZE);
-          return;
-        }
-      }
-    }
-  }
 
 
   const makeRxAwareContentUpdateFn = rxAware(setAnyContent);
@@ -737,23 +681,12 @@ var hyperHTML = (function (globalDocument, majinbuu) {'use strict';
     return wire[id] || (wire[id] = wireContent(type));
   }
 
-  // avoid processing too many nodes
-  // this is about the algorithm used
-  // to calculate the least amount of DOM
-  // changes needed to show the a new list
-  // where there was another one.
-  // There is a limit, in terms of performance,
-  // on how big can the optimal computation be,
-  // so if you change this value be sure your
-  // target hardware is good enough.
-  hyper.MAX_LIST_SIZE = 1000;
-
   // ---------------------------------------------
   // ⚡️ ️️The End ➰
   // ---------------------------------------------
   return hyper;
 
-}(document, majinbuu));
+}(document));
 
 export default hyperHTML;
 
