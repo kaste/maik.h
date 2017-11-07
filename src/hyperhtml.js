bf14fa8ad25ebcdb066e9c783e227709cfb04314
herr kaste <herr.kaste@gmail.com>
2017-11-07 23:25:39 +0100

    Linter/Prettier

--- src/hyperhtml.js
+++ src/hyperhtml.js
@@ -1,5 +1,3 @@
-/* eslint semi: [1, 'always'] */
-
 /*! (c) 2017 Andrea Giammarchi @WebReflection, (ISC) */
 /*! (c) 2017 herr.kaste, (ISC) */
 
@@ -7,165 +5,160 @@ import {
   upgrade,
   instantiateBlueprint,
   adoptBlueprint
-} from './make-template-instance.js';
-import { render, replaceNodeContent, extractContent } from './render.js';
-import { memoizeOnFirstArg, lruCacheOne, isArray } from './utils.js';
-import {$WeakMap} from './pseudo-polyfills.js';
-import {transformers, transformersKeys} from './node-updater.js';
-import {TagInvocation} from './tag-invocation-type.js';
+} from './make-template-instance.js'
+import { render, replaceNodeContent, extractContent } from './render.js'
+import { memoizeOnFirstArg, lruCacheOne, isArray } from './utils.js'
+import { $WeakMap } from './pseudo-polyfills.js'
+import { transformers, transformersKeys } from './node-updater.js'
+import { TagInvocation } from './tag-invocation-type.js'
 
-
-const OWNER_SVG_ELEMENT = 'ownerSVGElement';
-
-
-
+const OWNER_SVG_ELEMENT = 'ownerSVGElement'
 
 // ---------------------------------------------
 // hyperHTML Public API
 // ---------------------------------------------
 
-
-
-
 // The main render factories
 
 // hyper.bind(el) ⚡️
 // render TL inside a DOM node used as context
-export const bind = (node) => {
-  let finalSideEffect = lruCacheOne(replaceNodeContent.bind(null, node));
+export const bind = node => {
+  let finalSideEffect = lruCacheOne(replaceNodeContent.bind(null, node))
 
-  let document = node.ownerDocument;
-  let isSvg = OWNER_SVG_ELEMENT in node;
+  let document = node.ownerDocument
+  let isSvg = OWNER_SVG_ELEMENT in node
   let upgrader = memoizeOnFirstArg(
-    upgrade.bind(null, document, isSvg, instantiateBlueprint));
+    upgrade.bind(null, document, isSvg, instantiateBlueprint)
+  )
 
-  return render.bind(null, upgrader, finalSideEffect);
-};
+  return render.bind(null, upgrader, finalSideEffect)
+}
 
 export const html = (strings, ...values) => {
   if (isArray(strings)) {
-    return new TagInvocation(strings, values, false);
+    return new TagInvocation(strings, values, false)
   }
-  let key = strings;
-  return (strings, ...values) => new TagInvocation(strings, values, false, key);
-};
+  let key = strings
+  return (strings, ...values) =>
+    new TagInvocation(strings, values, false, key)
+}
 
 export const svg = (strings, ...values) => {
   if (isArray(strings)) {
-    return new TagInvocation(strings, values, true);
+    return new TagInvocation(strings, values, true)
   }
-  let key = strings;
-  return (strings, ...values) => new TagInvocation(strings, values, true, key);
-};
+  let key = strings
+  return (strings, ...values) =>
+    new TagInvocation(strings, values, true, key)
+}
 
-export const materialize = (tagInvocation) => {
-  return materializer()(tagInvocation);
-};
+export const materialize = tagInvocation => {
+  return materializer()(tagInvocation)
+}
 
-
-export const materializer = (finalizer=extractContent) => {
-  let finalSideEffect = lruCacheOne(finalizer);
-  let wire = lruCacheOne(
-    (isSvg) => memoizeOnFirstArg(
+export const materializer = (finalizer = extractContent) => {
+  let finalSideEffect = lruCacheOne(finalizer)
+  let wire = lruCacheOne(isSvg =>
+    memoizeOnFirstArg(
       upgrade.bind(null, document, isSvg, instantiateBlueprint)
     )
-  );
-  return (tagInvocation) => {
-    let {strings, values, isSvg} = tagInvocation;
-    let upgrader = wire(isSvg);
-    return render(upgrader, finalSideEffect, strings, ...values);
-  };
-};
+  )
+  return tagInvocation => {
+    let { strings, values, isSvg } = tagInvocation
+    let upgrader = wire(isSvg)
+    return render(upgrader, finalSideEffect, strings, ...values)
+  }
+}
 
 export const keyed = (key, tagInvocation) => {
-  tagInvocation.key = key;
-  return tagInvocation;
-};
+  tagInvocation.key = key
+  return tagInvocation
+}
 
 // hyper.wire(obj, 'type:ID') ➰
 // relate a renderer to a generic object
 export function wire(obj, type) {
-  return arguments.length < 1 ?
-    wireContent('html') :
-    (obj == null ?
-      wireContent(type || 'html') :
-      wireWeakly(obj, type || 'html')
-    );
+  return arguments.length < 1
+    ? wireContent('html')
+    : obj == null
+      ? wireContent(type || 'html')
+      : wireWeakly(obj, type || 'html')
 }
 
 export const wireHtml = () => {
-  let finalSideEffect = lruCacheOne(extractContent);
+  let finalSideEffect = lruCacheOne(extractContent)
   let upgrader = memoizeOnFirstArg(
-    upgrade.bind(null, document, false, instantiateBlueprint));
-  return render.bind(null, upgrader, finalSideEffect);
-};
+    upgrade.bind(null, document, false, instantiateBlueprint)
+  )
+  return render.bind(null, upgrader, finalSideEffect)
+}
 
 export const wireSvg = () => {
-  let finalSideEffect = lruCacheOne(extractContent);
+  let finalSideEffect = lruCacheOne(extractContent)
   let upgrader = memoizeOnFirstArg(
-    upgrade.bind(null, document, true, instantiateBlueprint));
-  return render.bind(null, upgrader, finalSideEffect);
-};
+    upgrade.bind(null, document, true, instantiateBlueprint)
+  )
+  return render.bind(null, upgrader, finalSideEffect)
+}
 
 // hyper.adopt(el) 🐣
 // adopt to an already live DOM structure
 // ATTENTION: Only works for flat templates
-export const adopt = (node) => {
-  let finalSideEffect = () => node;
+export const adopt = node => {
+  let finalSideEffect = () => node
 
-  let document = node.ownerDocument;
-  let isSvg = OWNER_SVG_ELEMENT in node;
-  let adopter = adoptBlueprint.bind(null, node);
+  let document = node.ownerDocument
+  let isSvg = OWNER_SVG_ELEMENT in node
+  let adopter = adoptBlueprint.bind(null, node)
   let upgrader = memoizeOnFirstArg(
-    upgrade.bind(null, document, isSvg, adopter));
+    upgrade.bind(null, document, isSvg, adopter)
+  )
 
-  return render.bind(null, upgrader, finalSideEffect);
-};
+  return render.bind(null, upgrader, finalSideEffect)
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - -
-
 
 // hyper.define('transformer', callback) 🌀
 export const define = (transformer, callback) => {
   if (!(transformer in transformers)) {
-    transformersKeys.push(transformer);
+    transformersKeys.push(transformer)
   }
-  transformers[transformer] = callback;
+  transformers[transformer] = callback
   // TODO: else throw ? console.warn ? who cares ?
-};
-
+}
 
 // ---------------------------------------------
 // Wires
 // ---------------------------------------------
 
 // [element] = {template, updates};
-var wires = new $WeakMap;
+var wires = new $WeakMap()
 
 // create a new wire for generic DOM content
 function wireContent(type) {
   switch (type) {
     case 'html':
-      return wireHtml();
+      return wireHtml()
     case 'svg':
-      return wireSvg();
+      return wireSvg()
   }
 }
 
 // setup a weak reference if needed and return a wire by ID
 function wireWeakly(obj, type) {
-  var wire = wires.get(obj);
-  var i = type.indexOf(':');
-  var id = type;
+  var wire = wires.get(obj)
+  var i = type.indexOf(':')
+  var id = type
   if (-1 < i) {
-    id = type.slice(i + 1);
-    type = type.slice(0, i) || 'html';
+    id = type.slice(i + 1)
+    type = type.slice(0, i) || 'html'
   }
   if (!wire) {
-    wire = {};
-    wires.set(obj, wire);
+    wire = {}
+    wires.set(obj, wire)
   }
-  return wire[id] || (wire[id] = wireContent(type));
+  return wire[id] || (wire[id] = wireContent(type))
 }
 
 // ---------------------------------------------
