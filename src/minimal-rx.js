d3239399e0fe73e3218fe70e8cd31f44a396948f
herr kaste <herr.kaste@gmail.com>
2017-11-28 17:21:47 +0100

    Remove minimal-rx

--- src/minimal-rx.js
+++
@@ -1,8 +0,0 @@
-import { Observable } from '../node_modules/rxjs-es/Observable'
-import { $$observable as observable } from '../node_modules/rxjs-es/symbol/observable'
-
-const Symbol = {
-  observable
-}
-const Rx = { Observable, Symbol }
-export default Rx
